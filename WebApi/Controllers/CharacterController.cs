0de01b139c310f9c1cecda79698afdd3c1a018e2
Martin Acerboni <macerboni@HEXACTA.COM>
2019-04-05 14:51:39 -0300

    controller creado

---
+++ WebApi/Controllers/CharacterController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class CharacterController : Controller
+    {
+
+        //public async Task<ActionResult<List<CheckedDailyForecast>>>
+
+        [HttpGet("[action]")]
+        public int Sports()
+        {
+            return 1;
+        }
+
+
+        [HttpGet("[action]/{sportName}/{neededDays}")]
+        public ActionResult<int> DailyForecasts(string sportName, int neededDays)
+        {
+            return 1;
+        }
+    }
+}
